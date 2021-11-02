22bdd9d9ffc0a5dbc8cb3259a005e22304070958
nico <nicorobledo92@hotmail.com>
2021-11-02 16:26:32 -0300

    Responsive

---
+++ src/Components/City.jsx
@@ -0,0 +1,13 @@
+import react from "react";
+import styled from "styled-components"; 
+import { useEffect } from "react";
+import { useParams } from "react-router";
+export default function City({props,cities}){
+
+    useParams()
+
+
+    return {
+        
+    }
+}
